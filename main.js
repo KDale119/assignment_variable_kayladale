6ef815a7fdfa26af382262defd51ac16ef5728be
Kayla Dale <kmeyers119@gmail.com>
2023-10-24 20:46:16 -0500

    created html & js added 4 variables & console.log

---
+++ main.js
@@ -0,0 +1,11 @@
+let color = 'brown';
+const hasLegs = true;
+const numOfLegs = 4;
+let catIs = 'working';
+
+console.log(typeof color + " " + color);
+console.log(typeof hasLegs + " " + hasLegs);
+console.log(typeof numOfLegs + " " + numOfLegs);
+console.log(typeof catIs + " " + catIs);
+
+console.log("The " + color + ' cat is ' + catIs + " and has " + numOfLegs + " legs.");
